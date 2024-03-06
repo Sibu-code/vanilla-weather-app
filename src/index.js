ae7b304068d748ca76ce21ee524b41308247cd89
Litha05 <gwayisibusisiwe@gmail.com>
2024-03-07 01:42:40 +0200

    Injected an icon, edited html and js
    Added a url for the icons and did some styling to make app look nicer

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new Date (response.data.time *1000);
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -14,6 +15,7 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-icon"/>`;
 }
 
 function formatDate(date) {
@@ -30,11 +32,11 @@ function formatDate(date) {
   ];
   let day = days[date.getDay()];
 
-  if (minutes <10){
-    minutes = 0 $ {minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
-  return  `${day} ${hours}:${minutes}`;
-  }
+  return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
   let apiKey = "cb967fdt435b979e0ef9f9d6a8ad3co5";
@@ -53,4 +55,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
-}
